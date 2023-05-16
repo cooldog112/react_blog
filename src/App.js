a785bf381b5d104d62674ce7a9ceab67f99cbb21
cooldog112 <48460609+cooldog112@users.noreply.github.com>
2023-05-16 17:54:12 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,36 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-
-let post = '공동교육과정'
+import data from './data.js';
 
 function App() {
-  let [title, setTitle] = useState(['스마트폰 추천', '웹 프로그래밍', '자바스크립트']);
-  let [like, setLike] = useState(0);
-
+  let [datas, setDatas] = useState(data);
+  let [modal, setModal] = useState(false);
+  let [index, setIndex] = useState(0);
   return (
     <div>
       <div className="black-nav">
-        <h3>블로그</h3>
+        <h3>매천고등학교 과목소개</h3>
       </div>
-      <button onClick={()=>{
-        setTitle(['공동교육과정', '웹 프로그래밍', '자바스크립트'])
-      }}>버튼</button>
-      <div className="list">
-        <h4>{title[0]} <span onClick={()=>{setLike(like+1)}}>좋아요 👍</span>{like}</h4>
-        <p>2023년 4월 11일</p>
-      </div>
-      <div className="list">
-        <h4>{title[1]}</h4>
-        <p>2023년 4월 11일</p>
-      </div>
-      <div className="list">
-        <h4>{title[2]}</h4>
-        <p>2023년 4월 11일</p>
-      </div>
-      
+      {
+        datas.map(function (row, i) {
+          return (
+            <div className="list">
+              <div className="title" onClick={() => {
+                modal ? setModal(false) : setModal(true);
+                setIndex(i);
+              }}>{row.title}</div>
+              <div className="contents">{row.date}</div>
+            </div>
+          )
+        })
+      }
+      {modal ? <Modal modal={modal}
+        setModal={setModal}
+        datas={datas}
+        setDatas={setDatas}
+        index={index}
+      ></Modal> : ''}
+
     </div>
   );
+}
+function Modal(props) {
+  let [modify, setModify] = useState(false);
+  let [contents, setContents] = useState('');
+  return (
+    <div className="modal">
+      <div className="modal-body">
+        <div className="modal-title">{props.datas[props.index].title}</div>
+        <div className="modal-date">{props.datas[props.index].date}
+          <span>👍</span>
+          <span>{props.datas[props.index].like}</span>
+        </div>
+        {modify ?
+          <div className="modal-contents">
+            <textarea className='modify-content' value={contents} />
+          </div> :
+          <div className="modal-contents">{props.datas[props.index].content}</div>
+        }
+
+        <button className="modal-button" onClick={() => {
+          props.modal ? props.setModal(false) : props.setModal(true);
+        }}>확인</button>
+        <button className='modal-button'
+          onClick={() => {
+            setModify(!modify);
+            setContents(props.datas[props.index].content)
+
+          }}>{modify ? '저장' : '수정'}</button>
+        <button className="modal-button" onClick={() => {
+          let copy = [...props.datas];
+          copy.splice(props.index, 1);
+          props.setDatas(copy);
+          props.modal ? props.setModal(false) : props.setModal(true);
+        }}>삭제</button>
+      </div>
+    </div>
+  )
 }
 
 export default App;
